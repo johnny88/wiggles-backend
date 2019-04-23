75823a3ba2f235fd9efe99fa66ed73c7f919a7df
John Pangalos <john.pangalos@hiq.se>
2019-04-23 12:13:42 +0200

    Add timestamp to image reference

--- functions/storage/afterImageUpload.js
+++ functions/storage/afterImageUpload.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const uuidv4 = require("uuid/v4");
+const path = require("path");
 
 const EXIT_MESSAGES = {
   OBJECT_DOESNT_EXIST: "Image object does not exist, exiting early.",
@@ -11,12 +12,17 @@ exports.afterImageUpload = functions.storage.object().onFinalize(object => {
   if (!object) return console.log(EXIT_MESSAGES.OBJECT_DOESNT_EXIST);
   if (!isObjectImage(object))
     return console.log(EXIT_MESSAGES.OBJECT_NOT_IMAGE);
+
   const id = uuidv4();
+  const timestamp = path.basename(filePath, path.extname(filePath));
   const imageRef = admin.database().ref(`images/${id}`);
+
   return imageRef.set({
     id,
     path: object.name,
-    contentType: object.contentType
+    contentType: object.contentType,
+    userId: object.metadata.userId,
+    timestamp
   });
 });
 
