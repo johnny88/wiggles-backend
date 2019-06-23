48794fd2990ef9bc4646ed19078f6c5e54b1e8eb
John Pangalos <john.pangalos@hiq.se>
2019-06-23 21:52:14 +0200

    Removing config...

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,5 @@
 const admin = require("firebase-admin");
 
-// var config = {
-  // apiKey: "AIzaSyAFp8tpwDeUhRFB4ruP-Wk6FyujzpGyon4",
-  // authDomain: "wiggles-f0bd9.firebaseapp.com",
-  // databaseURL: "https://wiggles-f0bd9.firebaseio.com",
-  // projectId: "wiggles-f0bd9",
-  // storageBucket: "wiggles-f0bd9.appspot.com",
-  // messagingSenderId: "837754270874",
-  // appId: "1:837754270874:web:4760e51978d04dfb"
-// };
-
 admin.initializeApp();
 
 exports.processSignUp = require("./auth/processSignUp").processSignUp;
