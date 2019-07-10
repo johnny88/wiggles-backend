fa1357b431d5a15f56e7769059d17c2fed4af140
John Pangalos <jpangs88@gmail.com>
2019-07-10 20:49:41 +0200

    Move to database email white list and add delete
    - Now fetching allowed-emails
    - Make sure users can't just log in...

--- functions/auth/processSignUp.js
+++ functions/auth/processSignUp.js
@@ -1,29 +1,24 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-const emailWhitelist = [
-  "jpangs88@gmail.com",
-  "susannebjorkblom@hotmail.com",
-  "lndpangalos@gmail.com",
-  "iris.pangalos@gmail.com",
-  "19pangalos@gmail.com",
-  "seanmheff@gmail.com",
-  "joan.manuel.valdes@gmail.com",
-  "gujehoglund@hotmail.com",
-  "karinbjorkblom@gmail.com",
-  "lasse.bjorkblom@hotmail.com"
-];
-const emailInWhiteList = ({ email, emailVerified }) =>
-  email && emailWhitelist.includes(email) && emailVerified;
-
 exports.processSignUp = functions.auth.user().onCreate(async user => {
+  const db = admin.firestore();
+
+  const allowedEmailsRef = await db.collection('allowed-emails').get();
+  const emailWhitelist = allowedEmailsRef.docs.map(item => item.data().email)
+
+  const emailInWhiteList = ({ email, emailVerified }) =>
+    email && emailWhitelist.includes(email) && emailVerified;
+
   if (!user) return console.log("User does not exist, exiting early.");
-  if (!emailInWhiteList(user))
-    return console.log(`${user.email} not in whitelist, exiting early.`);
+  if (!emailInWhiteList(user)) {
+    await admin.auth().deleteUser(user.uid);
+    console.log(`${user.email} not in whitelist, exiting early.`);
+    return;
+  }
   const customClaims = { authorized: true };
   try {
     console.log("Email in whitelist, creating account.");
-    const db = admin.firestore();
     const accountRef = db.collection('accounts').doc(user.uid);
     await accountRef.set({
       id: user.uid,
